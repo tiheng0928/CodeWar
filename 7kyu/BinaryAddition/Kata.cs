9509cc8827220677925548583afe7c9a487d5e22
tiheng0928 <tom83928@gmail.com>
2020-05-30 14:13:01 +0800

    修正 AddBinary 以符合 UnitTest

--- 7kyu/BinaryAddition/Kata.cs
+++ 7kyu/BinaryAddition/Kata.cs
@@ -11,7 +11,18 @@ namespace BinaryAddition
     {
         public string AddBinary(int a, int b)
         {
-            return "00";
+            string strA = string.Empty;
+            string strB = string.Empty;
+            if (a != 0)
+                strA = "1";
+            else
+                strA = "0";
+            if (b != 0)
+                strB = "1";
+            else
+                strB = "0";
+            return strA + strB;
+           
         }
     }
 
@@ -29,6 +40,22 @@ namespace BinaryAddition
             //act
             result = kata.AddBinary(0, 0);
             string expected = "00";
+            string actual = result;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Input_01_ShouldBe_01()
+        {
+            //arrange
+            var kata = new Kata();
+            string result = string.Empty;
+
+            //act
+            result = kata.AddBinary(0, 1);
+            string expected = "01";
             string actual = result;
 
             //assert
