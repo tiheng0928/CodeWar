0435d345ad01956c1f25b90206579a4b5940fa4a
tiheng0928 <tom83928@gmail.com>
2020-05-30 13:55:28 +0800

    Add 7kyu

---
+++ 7kyu/BinaryAddition/Kata.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BinaryAddition
+{
+    public class Kata
+    {
+        public string AddBinary(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [TestFixture]
+    public class BinaryAdditionTest
+    {
+        [Test]
+        public void Input_00_ShouldBe_00()
+        {
+            //arrange
+            var kata = new Kata();
+            string result = string.Empty;
+            
+
+            //act
+            result = kata.AddBinary(0, 0);
+            string expected = "00";
+            string actual = result;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
