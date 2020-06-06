4675b3cfef7ff939b19ce3365079a080bc3d1c17
tiheng0928 <tom83928@gmail.com>
2020-06-06 16:30:48 +0800

    [DescendingOrder] 新增測試案例，修改production code

--- 7kyu/DescendingOrder/Kata.cs
+++ 7kyu/DescendingOrder/Kata.cs
@@ -11,19 +11,48 @@ namespace DescendingOrder
     {
         public static int DescendingOrder(int num)
         {
-            return 0;
+            string InputStr = num.ToString();
+            int result;
+            if (InputStr.Length <= 1)
+                result = num;
+            else
+            {
+                int count = InputStr.Length;
+                var numchar = string.Empty;
+                for(int i = count; i > 0; i--)
+                {
+                    numchar += InputStr.Substring(i - 1, 1);
+                }
+                result = int.Parse(numchar);
+            }
+
+            return result;
         }
     }
 
     [TestFixture]
     public class DescendingOrder_Test
     {
+        [Test]
         public void Input_0_should_be_0()
         {
             //arrange
             var result = Kata.DescendingOrder(0);
             //act
             var expected = 0;
+            var actual = result;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Input_12_should_be_21()
+        {
+            //arrange
+            var result = Kata.DescendingOrder(12);
+            //act
+            var expected = 21;
             var actual = result;
 
             //assert
