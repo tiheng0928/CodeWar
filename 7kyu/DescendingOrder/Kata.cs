38c34c8dc8e3907f62685600ea384a5ccea40397
tiheng0928 <tom83928@gmail.com>
2020-06-06 16:06:24 +0800

    新增 DescendingOrder專案、gitignore檔案

---
+++ 7kyu/DescendingOrder/Kata.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DescendingOrder
+{
+    public class Kata
+    {
+        public static int DescendingOrder(int num)
+        {
+            return 0;
+        }
+    }
+
+    [TestFixture]
+    public class DescendingOrder_Test
+    {
+        public void Input_0_should_be_0()
+        {
+            //arrange
+            var result = Kata.DescendingOrder(0);
+            //act
+            var expected = 0;
+            var actual = result;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+}
