d14d2e3b5409c1c57a526519d37cd6878c39c95a
tiheng0928 <tom83928@gmail.com>
2020-06-13 14:38:22 +0800

    [ArrayDiff] 測試案例重構

--- 7kyu/ArrayDiff/Kata.cs
+++ 7kyu/ArrayDiff/Kata.cs
@@ -19,16 +19,27 @@ namespace ArrayDiff
     [TestFixture]
     public class ArrayDiffTest
     {
+        private Kata kata;
+        private int[] expected;
+        private int[] actual;
+        private int[] inputA;
+        private int[] inputB;
+
+        [SetUp]
+        public void SetUp()
+        {
+            kata = new Kata();
+        }
+
 
         [Test]
         public void Input_0_and_1_shouldbe_0()
         {
             //arrange
-            var kata = new Kata();
-            int[] expected;
-            int[] actual;
+            inputA = new int[] { 0 };
+            inputB = new int[] { 1 };
             //act
-            actual = kata.ArrayDiff(new int[] { 0 }, new int[] { 1 });
+            actual = kata.ArrayDiff(inputA,inputB);
             expected = new int[] { 0 };
             //assert
             Assert.AreEqual(expected, actual);
@@ -38,11 +49,10 @@ namespace ArrayDiff
         public void Input_0n1n2n3_and_0n2_shouldbe_1n3()
         {
             //arrange
-            var kata = new Kata();
-            int[] expected;
-            int[] actual;
+            inputA = new int[] { 0,1,2,3 };
+            inputB = new int[] { 0,2 };
             //act
-            actual = kata.ArrayDiff(new int[] { 0,1,2,3 }, new int[] { 0,2 });
+            actual = kata.ArrayDiff(inputA, inputB);
             expected = new int[] { 1,3 };
             //assert
             Assert.AreEqual(expected, actual);
