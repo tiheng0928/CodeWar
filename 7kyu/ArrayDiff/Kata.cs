4afa0d64f6a7f6db0d167fa52e9a31f976e2e09a
tiheng0928 <tom83928@gmail.com>
2020-06-13 14:29:05 +0800

    [ArrayDiff] 新增測試案例1

--- 7kyu/ArrayDiff/Kata.cs
+++ 7kyu/ArrayDiff/Kata.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +10,30 @@ namespace ArrayDiff
 {
     public class Kata
     {
+        public int[] ArrayDiff(int[] A, int[] B)
+        {
+            return A.Where(o => B.Contains(o) == false).Select(o => o).ToArray();
+        }
     }
 
+    [TestFixture]
+    public class ArrayDiffTest
+    {
+
+        [Test]
+        public void Input_0_and_1_shouldbe_0()
+        {
+            //arrange
+            var kata = new Kata();
+            int[] expected;
+            int[] actual;
+            //act
+            actual = kata.ArrayDiff(new int[] { 0 }, new int[] { 1 });
+            expected = new int[] { 0 };
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+    
 
 }
